dc2a67d33eaf5a5fb05c4b24a0c99b841949b8a1
FDisk80 <79803765+FDisk80@users.noreply.github.com>
2025-06-08 14:03:04 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+// This is a basic, empty service worker.
+// Its presence is enough to make the web app installable.
+self.addEventListener('fetch', (event) => {
+  // We are not adding any caching logic here for simplicity.
+  // The browser will handle fetching resources as usual.
+});
